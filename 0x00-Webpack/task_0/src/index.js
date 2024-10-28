fcc1dd1390305002cf57c2e96f65307b536876d4
gahbriehel <babatise002@gmail.com>
2024-10-28 17:33:37 +0100

    set up webpack and used jquery to render 3 paragraphs to index.html

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,7 @@
+import $ from "jquery";
+
+$(document).ready(() => {
+  $("body").append("<p>This is the first paragraph.</p>");
+  $("body").append("<p>This is the second paragraph.</p>");
+  $("body").append("<p>This is the third paragraph.</p>");
+});
