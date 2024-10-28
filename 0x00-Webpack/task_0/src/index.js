81aa0b02c66fb14d2e4694d618b592477ccfea16
gahbriehel <babatise002@gmail.com>
2024-10-28 17:57:00 +0100

    fixed jquery dependy issue

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,7 +1,7 @@
 import jQuery from "jQuery";
 
 jQuery(document).ready(function () {
-  jQuery("body").append("<p>First paragraph</p>");
-  jQuery("body").append("<p>Second paragraph</p>");
-  jQuery("body").append("<p>Third paragraph</p>");
+  jQuery("body").append("<p>Holberton Dashboard</p>");
+  jQuery("body").append("<p>Dashboard data for the students</p>");
+  jQuery("body").append("<p>Copyright - Holberton School</p>");
 });
