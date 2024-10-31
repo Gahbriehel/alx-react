cd66c2c321c92549fbe8ca9db55503cf0ebec8a7
gahbriehel <babatise002@gmail.com>
2024-10-31 04:50:49 +0100

    deleted comments

--- 0x01-react_intro/task_3/dashboard/src/setupTests.js
+++ 0x01-react_intro/task_3/dashboard/src/setupTests.js
@@ -6,7 +6,9 @@ import "@testing-library/jest-dom";
 
 // src/setupTests.js
 
-import Enzyme from "enzyme";
-import Adapter from "@cfaester/enzyme-adapter-react-18";
+// src/setupTests.js
 
-Enzyme.configure({ adapter: new Adapter() });
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-17";
+
+configure({ adapter: new Adapter() });
