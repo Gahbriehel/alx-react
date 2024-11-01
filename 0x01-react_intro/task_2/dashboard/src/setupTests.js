458dbe963647a45c342e7e689a244f6f4ac9bb40
gahbriehel <babatise002@gmail.com>
2024-11-01 09:32:24 +0100

    added readme file

--- 0x01-react_intro/task_2/dashboard/src/setupTests.js
+++ 0x01-react_intro/task_2/dashboard/src/setupTests.js
@@ -3,10 +3,3 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
-
-// src/setupTests.js
-
-import Enzyme from "enzyme";
-import Adapter from "@cfaester/enzyme-adapter-react-18";
-
-Enzyme.configure({ adapter: new Adapter() });
