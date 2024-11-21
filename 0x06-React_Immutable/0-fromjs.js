5e115a3c8528be60129dc841699806021497c8f3
gahbriehel <babatise002@gmail.com>
2024-11-21 23:29:43 +0100

    used es6 import

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,15 +1,6 @@
-
 import { fromJS } from 'immutable';
 
 export default function getImmutableObject(object) {
   return fromJS(object);
 }
 
-// const example = {
-//     fear: true,
-//     smell: -1033575916.9145899,
-//     wall: false,
-//     thing: -914767132
-// }
-
-// console.log(getImmutableObject(example))
