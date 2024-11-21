a73a33e2a47e26e477e2fbbd35b33e6cdd8e77fc
gahbriehel <babatise002@gmail.com>
2024-11-21 23:32:14 +0100

    used es6 import

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,6 +1,6 @@
 import { fromJS } from 'immutable';
 
-export default function getImmutableObject(object) {
-  return fromJS(object);
+export const getImmutableObject = (object) => {
+    return fromJS(object);
 }
 
